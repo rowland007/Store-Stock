ba6c5c7ef6caefdadec11fe7304f8fdc8891f085
Randy Rowland <randy.rowland@disroot.org>
2019-01-18 19:58:16 -0700

    Add initial source file for SupplyReorder class

---
+++ src/edu/snhu/assignments/SupplyReorder.java
@@ -0,0 +1,28 @@
+package edu.snhu.assignments;
+
+/******************************************************************************
+ Program:		Store Stock
+ Author:		Randall Rowland
+ Class:			IT 145 Foundations in App Development
+ Instructor:	Jim Barringer
+ Date:			18 Jan 2019
+ Description:   The Store Stock program requests the current on hand stock of
+                apples and oranges.  It also requests what the stock should be.
+                It will then output how many apples and oranges need to be
+                ordered to ensure a proper stock is maintained.
+ Input:         stdin
+ Output:        stdout
+
+ Known bugs/missing features:
+
+ License:       GNU General Public License v3.0
+ Modifications:
+ Date                Comment
+ ----    ------------------------------------------------
+ ******************************************************************************/
+
+public class SupplyReorder {
+    public static void main(String[] args) {
+    
+    }
+};
